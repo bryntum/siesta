c4570a42d77cbd4b4ea67a47112b34bd59a216ed
nickolay <nickolay8@gmail.com>
2020-10-28 18:16:39 +0300

    - some random progress

--- examples/nodejs/assertions.t.ts
+++ examples/nodejs/assertions.t.ts
@@ -1,4 +1,4 @@
-import { it } from "../../main.js"
+import { it, describe } from "../../main.js"
 import { Test as CustomTestClass } from "../../src/siesta/test/Test.js"
 
 it('Basic assertions should work', t => {
@@ -12,7 +12,7 @@ it('Basic assertions should work', t => {
 })
 
 
-it({
+describe({
     env         : 'generic',
     tags        : [ 'basic', 'generic', 'data', 'model' ],
     name        : 'Basic assertions should work',
@@ -25,5 +25,16 @@ it({
     t.is(1, 2, '1 is 2')
 
     t.is(new Date(2010, 1, 1), new Date(2010, 1, 1), 't.is works for dates')
+
+
+    t.it('Sub test', t => {
+        t.ok(true, 'True is ok')
+
+        t.is(null, undefined, 'Null is undefined')
+
+        t.is(1, 2, '1 is 2')
+
+        t.is(new Date(2010, 1, 1), new Date(2010, 1, 1), 't.is works for dates')
+    })
 })
 
