aec87474ccde45ba3de25548e8784692fcf56853
nickolay <nickolay8@gmail.com>
2020-12-02 17:54:54 +0300

    - simplified LoggerConsole

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -8,35 +8,37 @@ export enum LogLevel {
     error
 }
 
+export type LogMethod = 'info' | 'debug' | 'log' | 'warn' | 'error'
+
 export class Logger extends Base {
     logLevel        : LogLevel = LogLevel.warn
 
 
-    printLn (...message : string[]) {
+    printLn (method : LogMethod, ...message : string[]) {
     }
 
 
     info (...message : string[]) {
-        if (this.logLevel <= LogLevel.info) this.printLn(...message)
+        if (this.logLevel <= LogLevel.info) this.printLn('info', ...message)
     }
 
 
     debug (...message : string[]) {
-        if (this.logLevel <= LogLevel.debug) this.printLn(...message)
+        if (this.logLevel <= LogLevel.debug) this.printLn('debug', ...message)
     }
 
 
     log (...message : string[]) {
-        if (this.logLevel <= LogLevel.log) this.printLn(...message)
+        if (this.logLevel <= LogLevel.log) this.printLn('log', ...message)
     }
 
 
     warn (...message : string[]) {
-        if (this.logLevel <= LogLevel.warn) this.printLn(...message)
+        if (this.logLevel <= LogLevel.warn) this.printLn('warn', ...message)
     }
 
 
     error (...message : string[]) {
-        if (this.logLevel <= LogLevel.error) this.printLn(...message)
+        if (this.logLevel <= LogLevel.error) this.printLn('error', ...message)
     }
 }
--- src/logger/LoggerConsole.ts
+++ src/logger/LoggerConsole.ts
@@ -1,28 +1,8 @@
-import { Logger, LogLevel } from "./Logger.js"
+import { Logger, LogMethod } from "./Logger.js"
 
 export class LoggerConsole extends Logger {
 
-    info (...message : string[]) {
-        if (this.logLevel <= LogLevel.info) console.info(...message)
-    }
-
-
-    debug (...message : string[]) {
-        if (this.logLevel <= LogLevel.debug) console.debug(...message)
-    }
-
-
-    log (...message : string[]) {
-        if (this.logLevel <= LogLevel.log) console.log(...message)
-    }
-
-
-    warn (...message : string[]) {
-        if (this.logLevel <= LogLevel.warn) console.warn(...message)
-    }
-
-
-    error (...message : string[]) {
-        if (this.logLevel <= LogLevel.error) console.error(...message)
+    printLn (method : LogMethod, ...message : string[]) {
+        console[ method ](...message)
     }
 }
