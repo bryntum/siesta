11314f7e2ed02d8fc0239b92b35525c46b6e03dd
nickolay <nickolay8@gmail.com>
2020-12-30 15:52:06 +0300

    - TextBlock moved to Renderer

--- src/siesta/reporter/Reporter.tsx
+++ src/siesta/reporter/Reporter.tsx
@@ -1,7 +1,6 @@
 import { Base } from "../../class/Base.js"
 import { ClassUnion, Mixin } from "../../class/Mixin.js"
 import { LogLevel } from "../../logger/Logger.js"
-import { saneSplit } from "../../util/Helpers.js"
 import { relative } from "../../util/Path.js"
 import { SiestaJSX } from "../jsx/Factory.js"
 import { streamToElement, XmlElement, XmlStream } from "../jsx/XmlElement.js"
@@ -9,83 +8,8 @@ import { Launch } from "../project/Launch.js"
 import { Project } from "../project/Project.js"
 import { Assertion, AssertionAsyncResolution, LogMessage, Result, TestNodeResult, TestResult } from "../test/Result.js"
 import { Colorer } from "./Colorer.js"
-import { XmlRenderer } from "./XmlRenderer.js"
 import { randomSpinner, Spinner } from "./Spinner.js"
-
-//---------------------------------------------------------------------------------------------------------------------
-export class TextBlock extends Base {
-    text            : string[][]        = [ [] ]
-
-
-    addSameLineText (str : string) {
-        this.text[ this.text.length - 1 ].push(str)
-    }
-
-
-    addNewLine () {
-        this.text.push([])
-    }
-
-
-    push (...strings : string[]) {
-        strings.forEach(string => saneSplit(string, '\n').forEach((str, index, array) => {
-            this.addSameLineText(str)
-
-            if (index !== array.length - 1) this.addNewLine()
-        }))
-    }
-
-
-    pushLn (...strings : string[]) {
-        this.push(...strings, '\n')
-    }
-
-
-    pullFrom (another : TextBlock) {
-        const [ firstLine, ...otherLines ]  = another.text
-
-        this.text[ this.text.length - 1 ].push(...firstLine)
-
-        this.text.push(...otherLines)
-    }
-
-
-    toString () : string {
-        return this.text.map(parts => parts.join('')).join('\n')
-    }
-
-
-    colorizeMut (c : Colorer) {
-        this.text.forEach((line, index) => this.text[ index ] = [ c.text(line.join('')) ])
-    }
-
-
-    indentMut (howMany : number, includeMarker : boolean = true) {
-        const indenter  = ' '.repeat(howMany)
-
-        this.text.forEach((line, index) => {
-            if (index === 0 && includeMarker) {
-                line.unshift('·' + indenter.slice(1))
-            } else {
-                line.unshift(indenter)
-            }
-        })
-    }
-
-
-    indentAsTreeLeafMut (howMany : number, isLast : boolean, c : Colorer) {
-        const indenterPlain     = ' '.repeat(howMany - 1)
-        const indenterTree      = '─'.repeat(howMany - 1)
-
-        this.text.forEach((line, index) => {
-            if (index === 0) {
-                line.unshift(c.text(isLast ? '└' + indenterTree : '├' + indenterTree))
-            } else {
-                line.unshift(c.text(isLast ? ' ' + indenterPlain : '│' + indenterPlain))
-            }
-        })
-    }
-}
+import { XmlRenderer } from "./XmlRenderer.js"
 
 
 //---------------------------------------------------------------------------------------------------------------------
--- src/siesta/reporter/XmlRenderer.ts
+++ src/siesta/reporter/XmlRenderer.ts
@@ -4,8 +4,84 @@ import { saneSplit } from "../../util/Helpers.js"
 import { isString } from "../../util/Typeguards.js"
 import { XmlElement, XmlNode } from "../jsx/XmlElement.js"
 import { Colorer, ColorerRule } from "./Colorer.js"
-import { TextBlock } from "./Reporter.js"
 import { styles } from "./styling/terminal.js"
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export class TextBlock extends Base {
+    text            : string[][]        = [ [] ]
+
+
+    addSameLineText (str : string) {
+        this.text[ this.text.length - 1 ].push(str)
+    }
+
+
+    addNewLine () {
+        this.text.push([])
+    }
+
+
+    push (...strings : string[]) {
+        strings.forEach(string => saneSplit(string, '\n').forEach((str, index, array) => {
+            this.addSameLineText(str)
+
+            if (index !== array.length - 1) this.addNewLine()
+        }))
+    }
+
+
+    pushLn (...strings : string[]) {
+        this.push(...strings, '\n')
+    }
+
+
+    pullFrom (another : TextBlock) {
+        const [ firstLine, ...otherLines ]  = another.text
+
+        this.text[ this.text.length - 1 ].push(...firstLine)
+
+        this.text.push(...otherLines)
+    }
+
+
+    toString () : string {
+        return this.text.map(parts => parts.join('')).join('\n')
+    }
+
+
+    colorizeMut (c : Colorer) {
+        this.text.forEach((line, index) => this.text[ index ] = [ c.text(line.join('')) ])
+    }
+
+
+    indentMut (howMany : number, includeMarker : boolean = true) {
+        const indenter              = ' '.repeat(howMany)
+        const indenterWithMarker    = ' '.repeat(howMany - 2) + '· '
+
+        this.text.forEach((line, index) => {
+            if (index === 0 && includeMarker) {
+                line.unshift(indenterWithMarker)
+            } else {
+                line.unshift(indenter)
+            }
+        })
+    }
+
+
+    indentAsTreeLeafMut (howMany : number, isLast : boolean, c : Colorer) {
+        const indenterPlain     = ' '.repeat(howMany - 1)
+        const indenterTree      = '─'.repeat(howMany - 1)
+
+        this.text.forEach((line, index) => {
+            if (index === 0) {
+                line.unshift(c.text(isLast ? '└' + indenterTree : '├' + indenterTree))
+            } else {
+                line.unshift(c.text(isLast ? ' ' + indenterPlain : '│' + indenterPlain))
+            }
+        })
+    }
+}
 
 
 //---------------------------------------------------------------------------------------------------------------------
