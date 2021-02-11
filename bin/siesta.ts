a1b0be0c112cf9529f982834e0481f4c49ac7e7e
nickolay <nickolay8@gmail.com>
2021-02-11 13:36:28 +0300

    - downgrade to node 12

--- bin/siesta.ts
+++ bin/siesta.ts
@@ -7,14 +7,20 @@ process.on('unhandledRejection', (reason, promise) => {
     console.log('Unhandled promise rejection, reason:', reason)
 
     process.exit(ExitCodes.UNHANLED_EXCEPTION)
-})
+});
+
+(async () => {
 
 const launcher  = LauncherNodejs.new({
     inputArguments      : process.argv.slice(2)
 })
+
 
 const launch        = await launcher.start()
 
 // we just set the `exitCode` property and not call `process.exit()` directly,
 // because some output might not be processed yet
 process.exitCode    = process.exitCode ?? launch.getExitCode()
+
+})()
+
