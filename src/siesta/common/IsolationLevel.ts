d33a2817803e972e10bd2d8142b6397815bfe328
nickolay <nickolay8@gmail.com>
2021-03-30 14:26:12 +0300

    - todo

--- src/siesta/common/IsolationLevel.ts
+++ src/siesta/common/IsolationLevel.ts
@@ -1,52 +1,52 @@
-import { Base } from "../../class/Base.js"
-import { ClassUnion, Mixin } from "../../class/Mixin.js"
-
-/*
-
-iso
----
-SameContext
-NewContext
-
-
-node
-----
-NewProcess
-
-
-browser
--------
-Popup
-
-
-*/
-
-
-//---------------------------------------------------------------------------------------------------------------------
-export class IsolationLevel extends Mixin(
-    [ Base ],
-    (base : ClassUnion<typeof Base>) =>
-
-    class IsolationLevel extends base {
-    }
-) {}
-
-
-//---------------------------------------------------------------------------------------------------------------------
-export class SameContext extends Mixin(
-    [ IsolationLevel ],
-    (base : ClassUnion<typeof IsolationLevel>) =>
-
-    class SameContext extends base {
-    }
-) {}
-
-
-//---------------------------------------------------------------------------------------------------------------------
-export class NodejsContext extends Mixin(
-    [ IsolationLevel ],
-    (base : ClassUnion<typeof IsolationLevel>) =>
-
-    class NodejsContext extends base {
-    }
-) {}
+// import { Base } from "../../class/Base.js"
+// import { ClassUnion, Mixin } from "../../class/Mixin.js"
+//
+// /*
+//
+// iso
+// ---
+// SameContext
+// NewContext
+//
+//
+// node
+// ----
+// NewProcess
+//
+//
+// browser
+// -------
+// Popup
+//
+//
+// */
+//
+//
+// //---------------------------------------------------------------------------------------------------------------------
+// export class IsolationLevel extends Mixin(
+//     [ Base ],
+//     (base : ClassUnion<typeof Base>) =>
+//
+//     class IsolationLevel extends base {
+//     }
+// ) {}
+//
+//
+// //---------------------------------------------------------------------------------------------------------------------
+// export class SameContext extends Mixin(
+//     [ IsolationLevel ],
+//     (base : ClassUnion<typeof IsolationLevel>) =>
+//
+//     class SameContext extends base {
+//     }
+// ) {}
+//
+//
+// //---------------------------------------------------------------------------------------------------------------------
+// export class NodejsContext extends Mixin(
+//     [ IsolationLevel ],
+//     (base : ClassUnion<typeof IsolationLevel>) =>
+//
+//     class NodejsContext extends base {
+//     }
+// ) {}
