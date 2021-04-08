65773c44d8be5f471d9425381da16986b5d346a2
nickolay <nickolay8@gmail.com>
2021-04-08 16:53:28 +0300

    - whitespace

--- src/rpc/media/MediaSerializable.ts
+++ src/rpc/media/MediaSerializable.ts
@@ -1,6 +1,6 @@
 import { ClassUnion, Mixin } from "../../class/Mixin.js"
 import { parse, stringify } from "../../serializable/Serializable.js"
-import { EnvelopCall, EnvelopResult} from "../port/Port.js"
+import { EnvelopCall, EnvelopResult } from "../port/Port.js"
 import { Media } from "./Media.js"
 
 //---------------------------------------------------------------------------------------------------------------------
