d2856b0d14a5c8c819fcaa3fdbe297685701f66e
nickolay <nickolay8@gmail.com>
2021-04-13 18:27:25 +0300

    - selective override for ExecutionContextNode

--- src/context/ExecutionContextNode.ts
+++ src/context/ExecutionContextNode.ts
@@ -5,6 +5,10 @@ import { ExecutionContext } from "./ExecutionContext.js"
 //---------------------------------------------------------------------------------------------------------------------
 export class ExecutionContextNode extends ExecutionContext {
 
+    overrideConsole     : boolean       = true
+    overrideException   : boolean       = true
+    overrideOutput      : boolean       = true
+
     uncaughtExceptionListener   : AnyFunction       = undefined
     uncaughtRejectionListener   : AnyFunction       = undefined
 
@@ -21,57 +25,69 @@ export class ExecutionContextNode extends ExecutionContext {
 
 
     setup () {
-        this.uncaughtExceptionListener  = (err : unknown, origin : 'uncaughtException' | 'uncaughtRejection') => {
-            this.onException(err, origin === 'uncaughtException' ? 'exception' : 'rejection')
-        }
-        this.uncaughtRejectionListener  = (err) => {
-            this.onException(err, 'rejection')
-        }
+        if (this.overrideException) {
+            this.uncaughtExceptionListener  = (err : unknown, origin : 'uncaughtException' | 'uncaughtRejection') => {
+                this.onException(err, origin === 'uncaughtException' ? 'exception' : 'rejection')
+            }
+            this.uncaughtRejectionListener  = (err) => {
+                this.onException(err, 'rejection')
+            }
 
-        process.on('unhandledRejection', this.uncaughtRejectionListener)
-        process.on('uncaughtException', this.uncaughtExceptionListener)
-
-        this.consoleOriginals   = {
-            error   : console.error,
-            warn    : console.warn,
-            log     : console.log,
-            debug   : console.debug,
-            info    : console.info,
+            process.on('unhandledRejection', this.uncaughtRejectionListener)
+            process.on('uncaughtException', this.uncaughtExceptionListener)
         }
 
-        Object.assign(console, {
-            error   : (...message) => this.onConsole('error', ...message),
-            warn    : (...message) => this.onConsole('warn', ...message),
-            log     : (...message) => this.onConsole('log', ...message),
-            debug   : (...message) => this.onConsole('debug', ...message),
-            info    : (...message) => this.onConsole('info', ...message),
-        })
+        if (this.overrideConsole) {
+            this.consoleOriginals   = {
+                error   : console.error,
+                warn    : console.warn,
+                log     : console.log,
+                debug   : console.debug,
+                info    : console.info,
+            }
 
-        this.stdOutWriteOriginal    = process.stdout.write
-        this.stdErrWriteOriginal    = process.stderr.write
-
-        process.stdout.write = (buffer) => {
-            this.onOutput('stdout', buffer)
-
-            return true
+            Object.assign(console, {
+                error   : (...message) => this.onConsole('error', ...message),
+                warn    : (...message) => this.onConsole('warn', ...message),
+                log     : (...message) => this.onConsole('log', ...message),
+                debug   : (...message) => this.onConsole('debug', ...message),
+                info    : (...message) => this.onConsole('info', ...message),
+            })
         }
-        process.stderr.write = (buffer) => {
-            this.onOutput('stderr', buffer)
 
-            return true
+        if (this.overrideOutput) {
+            this.stdOutWriteOriginal = process.stdout.write
+            this.stdErrWriteOriginal = process.stderr.write
+
+            process.stdout.write = (buffer) => {
+                this.onOutput('stdout', buffer)
+
+                return true
+            }
+            process.stderr.write = (buffer) => {
+                this.onOutput('stderr', buffer)
+
+                return true
+            }
         }
     }
 
 
     destroy () {
-        process.off('unhandledRejection', this.uncaughtRejectionListener)
-        process.off('uncaughtException', this.uncaughtExceptionListener)
+        if (this.overrideException) {
+            process.off('unhandledRejection', this.uncaughtRejectionListener)
+            process.off('uncaughtException', this.uncaughtExceptionListener)
 
-        this.uncaughtRejectionListener  = this.uncaughtExceptionListener = undefined
+            this.uncaughtRejectionListener  = this.uncaughtExceptionListener = undefined
+        }
 
-        Object.assign(console, this.consoleOriginals)
+        if (this.overrideConsole) {
+            Object.assign(console, this.consoleOriginals)
+        }
 
-        process.stdout.write = this.stdOutWriteOriginal
-        process.stderr.write = this.stdErrWriteOriginal
+        if (this.overrideOutput) {
+            process.stdout.write = this.stdOutWriteOriginal
+            process.stderr.write = this.stdErrWriteOriginal
+        }
     }
 }
