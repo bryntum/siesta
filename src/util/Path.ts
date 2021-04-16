46f4ddeb31ecc538e96d5ec9cd440cf0586b0912
nickolay <nickolay8@gmail.com>
2021-04-16 18:49:51 +0300

    - various path helper methods, starting to emerge into path module

--- src/util/Path.ts
+++ src/util/Path.ts
@@ -1,10 +1,14 @@
+// TODO this module is currently just a quick&dirty implementation, needs proper refactoring,
+// since the WHATWG URL implementation is a total crap (lacks any useful methods,
+// like `isAbsolute`, `join`, etc
 
-// export const isAbsolute = (url : string) : boolean => {
-//     return new URL(url, "http://base/").href === url
-// }
+//---------------------------------------------------------------------------------------------------------------------
+export const isAbsolute = (url : string) : boolean => {
+    return /^(https?:|file:\/\/\/|\/)/i.test(url)
+}
 
 
-// TODO just a stub for now
+//---------------------------------------------------------------------------------------------------------------------
 export const relative = (baseUrl : string, url : string) : string => {
     baseUrl = baseUrl.replace(/\/?$/, '/')
 
@@ -13,4 +17,26 @@ export const relative = (baseUrl : string, url : string) : string => {
     if (indexOf === 0) return url.slice(baseUrl.length)
 
     return url
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+export const stripBasename = (url : string, keepTrailingSlash : boolean = true) : string =>
+    url.replace(/\/[^/]*?$/, keepTrailingSlash ? '/' : '')
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export const stripDirname = (url : string) : string => url.replace(/^.*\//, '')
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export const stripTrailingSlash = (url : string) : string => url.replace(/\/$/, '')
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export const ensureTrailingSlash = (url : string) : string => url.replace(/\/$/, '/')
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export const joinUrls = (baseUrl : string, relativeUrl : string) : string => {
+    return isAbsolute(relativeUrl) ? relativeUrl : ensureTrailingSlash(baseUrl) + relativeUrl
 }
