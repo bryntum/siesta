0ea1da7a0ccecc65e928b4cc50ccedc0da490fc4
nickolay <nickolay8@gmail.com>
2021-04-21 12:50:07 +0300

    - WebWorker media

---
+++ src/rpc/media/MediaWebWorker.ts
@@ -0,0 +1,75 @@
+import { Serializable } from "child_process"
+import { Base } from "../../class/Base.js"
+import { ClassUnion, Mixin } from "../../class/Mixin.js"
+import { MediaSerializableJSON } from "./MediaSerializable.js"
+
+//---------------------------------------------------------------------------------------------------------------------
+declare const self : DedicatedWorkerGlobalScope
+
+//---------------------------------------------------------------------------------------------------------------------
+export class MediaWebWorkerChild extends Mixin(
+    [ MediaSerializableJSON, Base ],
+    (base : ClassUnion<typeof MediaSerializableJSON, typeof Base>) =>
+
+    class MediaWebWorkerChild extends base {
+
+        messageListener         : (...args : any[]) => void     = undefined
+
+        workerGlobalScope       : DedicatedWorkerGlobalScope    = self
+
+
+        async doConnect () : Promise<any> {
+            this.workerGlobalScope.addEventListener('message', this.messageListener = (e : MessageEvent) => {
+                this.receiveMessage(e.data)
+            })
+        }
+
+
+        async doDisconnect () : Promise<any> {
+            this.workerGlobalScope.removeEventListener('message', this.messageListener)
+
+            this.messageListener    = undefined
+
+            this.workerGlobalScope.close()
+        }
+
+
+        sendMessage (message : any) {
+            this.workerGlobalScope.postMessage(message)
+        }
+    }
+){}
+
+
+//---------------------------------------------------------------------------------------------------------------------
+export class MediaWebWorkerParent extends Mixin(
+    [ MediaSerializableJSON, Base ],
+    (base : ClassUnion<typeof MediaSerializableJSON, typeof Base>) =>
+
+    class MediaWebWorkerParent extends base {
+        worker                  : Worker                        = undefined
+
+        messageListener         : (...args : any[]) => void     = undefined
+
+
+        async doConnect () : Promise<any> {
+            this.worker.addEventListener('message', this.messageListener = (message : MessageEvent) => {
+                this.receiveMessage(message.data)
+            })
+        }
+
+
+        async doDisconnect () : Promise<any> {
+            this.worker.removeEventListener('message', this.messageListener)
+
+            this.messageListener    = undefined
+
+            this.worker.terminate()
+        }
+
+
+        sendMessage (message : Serializable) {
+            this.worker.postMessage(message)
+        }
+    }
+){}
