ad4324594ab41fe189e8a07924328552d76ba1fe
nickolay <nickolay8@gmail.com>
2021-05-05 11:18:21 +0300

    - Deno terminal uses `peformPrint` helper

--- src/siesta/reporter/TerminalDeno.ts
+++ src/siesta/reporter/TerminalDeno.ts
@@ -6,33 +6,37 @@ import { Terminal } from "./Terminal.js"
 export class TerminalDeno extends Terminal {
 
     showCursor () {
-        showCursor()
+        this.launcher.performPrint(() => showCursor())
     }
 
     hideCursor () {
-        hideCursor()
+        this.launcher.performPrint(() => hideCursor())
     }
 
 
     moveCursor (dx : number, dy : number) {
-        if (dx > 0)
-            goRight(dx)
-        else if (dx < 0)
-            goLeft(-dx)
+        this.launcher.performPrint(() => {
+            if (dx > 0)
+                goRight(dx)
+            else if (dx < 0)
+                goLeft(-dx)
 
-        if (dy > 0)
-            goDown(dy)
-        else if (dy < 0)
-            goUp(-dy)
+            if (dy > 0)
+                goDown(dy)
+            else if (dy < 0)
+                goUp(-dy)
+        })
     }
 
 
     clearLine (dir : 'left' | 'right' | 'line') {
-        if (dir === 'left')
-            clearLeft()
-        else if (dir === 'right')
-            clearRight()
-        else
-            clearLine()
+        this.launcher.performPrint(() => {
+            if (dir === 'left')
+                clearLeft()
+            else if (dir === 'right')
+                clearRight()
+            else
+                clearLine()
+        })
     }
 }
