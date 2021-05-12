9d2dd7777313686c22681d656719c2cddc1759a6
nickolay <nickolay8@gmail.com>
2021-05-12 17:26:47 +0300

    - stub for release script

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# exit if any of command has failed
+set -e
+
+# switch to the root folder of the package
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd "$DIR/.."
+
+# TODO change the guide URL in the
+# examples/nodejs/README.md
