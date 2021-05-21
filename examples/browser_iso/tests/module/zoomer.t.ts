ad172eb5511f7d16b48008b8c8456e0c400f96bf
nickolay <nickolay8@gmail.com>
2021-05-21 14:56:23 +0300

    - browser_iso example deleted

--- examples/browser_iso/tests/module/zoomer.t.ts
+++
@@ -1,8 +0,0 @@
-import { it, Test } from "../../../../index.js"
-import { zoomer } from "../../src/module.js"
-
-it('Using `zoomer` should work', async (t : Test) => {
-
-    t.equal(zoomer('Myers Courtney', 35), { name : 'Courtney Myers', age : 35 })
-})
-
