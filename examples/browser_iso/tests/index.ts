80bf64c92164c1bddef51da75e44c09e5f480a1f
nickolay <nickolay8@gmail.com>
2021-05-21 14:32:33 +0300

    - browser_iso example

---
+++ examples/browser_iso/tests/index.ts
@@ -0,0 +1,28 @@
+import { Project } from "../../../index.js"
+
+const project = Project.new({
+    title                   : 'Awesome isomorphic project test suite',
+})
+
+// currently, isomorphic project does not have access to file system
+// it needs to list all test files manually
+project.plan(
+    {
+        filename        : 'basic',
+
+        items           : [
+            'basic_test.t.js'
+        ]
+    },
+    {
+        filename        : 'module',
+
+        items           : [
+            'delay.t.js',
+            'summer.t.js',
+            'zoomer.t.js'
+        ]
+    }
+)
+
+project.start()
---
+++ examples/browser_iso/tests/module/zoomer.t.ts
@@ -0,0 +1,8 @@
+import { it, Test } from "../../../../index.js"
+import { zoomer } from "../../src/module.js"
+
+it('Using `zoomer` should work', async (t : Test) => {
+
+    t.equal(zoomer('Myers Courtney', 35), { name : 'Courtney Myers', age : 35 })
+})
+
