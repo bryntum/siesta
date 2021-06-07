085d63c168a0dffc7d96953effdc75778fa4b3b4
nickolay <nickolay8@gmail.com>
2021-06-07 18:23:19 +0300

    - build script

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 
 # exit if any of command has failed
 set -e
-# enable ** in globs
+# enable **, ! in globs
 shopt -s globstar extglob
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -31,16 +31,16 @@ if [[ -n $release ]]; then
     # we don't need to add type references for files in `bin/`
     GLOBIGNORE=@(examples|node_modules|bin)/**
 
-#    for filename_ts in **/!(*.d).ts?(x); do
-#        regexp="(.*)\.tsx?"
-#
-#        if [[ $filename_ts =~ $regexp ]]; then
-#            filename_js="${BASH_REMATCH[1]}.js"
-#            filename_types=$(basename "${BASH_REMATCH[1]}.d.ts")
-#
-#            awk -i inplace -v FILENAME_TYPES="$filename_types" 'BEGINFILE{printf "/// <reference types=\"./%s\" />\n",FILENAME_TYPES}{print}' $filename_js
-#        fi
-#    done
+    for filename_ts in **/!(*.d).ts?(x); do
+        regexp="(.*)\.tsx?"
+
+        if [[ $filename_ts =~ $regexp ]]; then
+            filename_js="${BASH_REMATCH[1]}.js"
+            filename_types=$(basename "${BASH_REMATCH[1]}.d.ts")
+
+            awk -i inplace -v FILENAME_TYPES="$filename_types" 'BEGINFILE{printf "/// <reference types=\"./%s\" />\n",FILENAME_TYPES}{print}' $filename_js
+        fi
+    done
 
     # we need to remove the *.ts files everywhere, including the `bin/`
     GLOBIGNORE=@(examples|node_modules)/**
