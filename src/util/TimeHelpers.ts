cdc676470b66a2860bdcd3207ba477dcfa1aad2b
nickolay <nickolay8@gmail.com>
2021-08-11 17:38:51 +0300

    - comments

--- src/util/TimeHelpers.ts
+++ src/util/TimeHelpers.ts
@@ -1,10 +1,10 @@
-//---------------------------------------------------------------------------------------------------------------------
 import { SetTimeoutHandler } from "./Helpers.js"
 
+//---------------------------------------------------------------------------------------------------------------------
 export const delay = (timeout : number) : Promise<any> => new Promise(resolve => setTimeout(resolve, timeout))
 
 //---------------------------------------------------------------------------------------------------------------------
-// it is recommended that the error instance, to throw on timeout, to be provided at the call site of this method
+// it is recommended, that the error instance, to throw on timeout, to be provided from the call site of this method
 // this way, the stack trace will point to the `timeout` call, instead of the `timeout` internals
 export const timeout = <T>(promise : Promise<T>, timeout : number, error : Error = new Error(`Timeout of ${ timeout }ms exceeded`)) : Promise<T> => {
 
