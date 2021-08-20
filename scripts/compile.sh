4e0d90775737328aaf40f6233b1c0f5c9ad94f7d
nickolay <nickolay8@gmail.com>
2021-08-20 12:11:47 +0300

    - 'compile' scripts

---
+++ scripts/compile.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# exit early if any of the commands failed
+set -e
+# enable **, ! in globs
+shopt -s globstar extglob
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd "$DIR/.."
+
+(
+    cd packages/chained-iterator
+    echo ""
+    echo "------------------------------------------"
+    echo "Compiling [chained-iterator]"
+    npx tsc
+)
+
+(
+    cd packages/typescript-mixin-class
+    echo ""
+    echo "------------------------------------------"
+    echo "Compiling [typescript-mixin-class]"
+    npx tsc
+)
+
+(
+    cd packages/typescript-serializable-mixin
+    echo ""
+    echo "------------------------------------------"
+    echo "Compiling [typescript-serializable-mixin]"
+    npx tsc
+)
+
+(
+    cd packages/siesta
+    echo ""
+    echo "------------------------------------------"
+    echo "Compiling [siesta]"
+    npx tsc
+)
