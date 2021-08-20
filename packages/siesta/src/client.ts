1a970a90f06838d549264fe80bbab946a20f418a
nickolay <nickolay8@gmail.com>
2021-08-20 12:13:41 +0300

    - bug reproduce

---
+++ packages/siesta/src/client.ts
@@ -0,0 +1,37 @@
+import { MediaBrowserWebSocketChild } from "./rpc/media/MediaBrowserWebSocketChild.js"
+import { SimulatorPlaywrightClient } from "./siesta/simulate/SimulatorPlaywright.js"
+import { awaitDomReady } from "./util/Helpers.js"
+import { delay } from "./util/TimeHelpers.js"
+
+
+const connect = async (wsPort : number) => {
+    await awaitDomReady()
+
+    // const div           = document.getElementById('canvas')
+    //
+    // div.addEventListener('mousemove', () => console.log("MOUSE ENTER"))
+
+    const port          = SimulatorPlaywrightClient.new()
+    const media         = MediaBrowserWebSocketChild.new({ wsHost : '127.0.0.1', wsPort : wsPort })
+
+    port.media          = media
+
+    port.handshakeType  = 'parent_first'
+
+    await port.connect()
+
+    await delay(1000)
+
+    await port.mouseMove([ 50, 50 ])
+
+    await delay(500)
+}
+
+console.log("FROM CLIENT")
+
+// @ts-ignore
+window.connect = port => {
+    connect(port)
+}
+
+
---
+++ packages/siesta/src/server.ts
@@ -0,0 +1,54 @@
+import playwright from "playwright"
+import ws from "ws"
+import { MediaNodeWebSocketParent } from "./rpc/media/MediaNodeWebSocketParent.js"
+import { ServerNodeWebSocket } from "./rpc/server/ServerNodeWebSocket.js"
+import { SimulatorPlaywrightServer } from "./siesta/simulate/SimulatorPlaywright.js"
+
+
+const run = async () => {
+    const browser           = await playwright.webkit.launch({ headless : false, /*devtools : true, args : [ '--start-maximized' ]*/ })
+
+    const wsServer          = new ServerNodeWebSocket()
+
+    const wsPort            = await wsServer.startWebSocketServer()
+
+    const awaitConnection   = new Promise<ws>(resolve => wsServer.onConnectionHook.once((self, socket) => resolve(socket)))
+
+    const page              = await browser.newPage({ viewport : null })
+
+    page.on('console', async msg => {
+        for (let i = 0; i < msg.args().length; i++)
+            console.log(`${ i }: ${ await msg.args()[ i ].jsonValue() }`)
+        }
+    )
+
+    page.on('pageerror', e => console.log(e))
+
+    const port              = SimulatorPlaywrightServer.new({ page })
+    const media             = MediaNodeWebSocketParent.new()
+
+    port.media              = media
+
+    await page.goto('http://localhost:8000/src/index.html')
+
+    await page.waitForLoadState('load')
+
+    // // @ts-ignore
+    // await page.waitForFunction(() => window.connect !== undefined)
+
+    console.log("GOTO DONE")
+
+    await page.evaluate(`window.connect(${ wsPort })`)
+
+    console.log("EVAL DONE")
+
+    media.socket            = await awaitConnection
+
+    port.handshakeType      = 'parent_first'
+
+    await port.connect()
+
+    console.log("CONNECTED")
+}
+
+run()
