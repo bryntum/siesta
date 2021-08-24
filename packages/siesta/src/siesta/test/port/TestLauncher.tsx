7673ee6422189237e3658d3666613370fb9ec834
nickolay <nickolay8@gmail.com>
2021-08-24 13:08:53 +0300

    - cleanup

--- packages/siesta/src/siesta/test/port/TestLauncher.tsx
+++ packages/siesta/src/siesta/test/port/TestLauncher.tsx
@@ -9,22 +9,10 @@ import { globalTestEnv, Test } from "../Test.js"
 import { SubTestCheckInfo } from "../TestResult.js"
 import { TestReporterChild, TestReporterParent } from "./TestReporter.js"
 
-// //---------------------------------------------------------------------------------------------------------------------
-// // make sure we actually import these class symbols (and not just types),
-// // so that their `registerSerializableClass()` calls are made
-//
-// TestDescriptor
-// // IMPORTANT the following classes are assumed to be isomorphic by themselves
-// // (even that they represent the data for non-isomorphic classes)
-// TestDescriptorNodejs
-// TestDescriptorDeno
-// TestDescriptorBrowser
 
 //---------------------------------------------------------------------------------------------------------------------
 interface TestLauncher {
     launchTest (testDescriptorStr : string, checkInfo : SubTestCheckInfo) : Promise<any>
-
-    // getSameContextChildLauncher () : Promise<TestLauncherChild>
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -32,14 +20,10 @@ export class TestLauncherParent extends Mixin(
     [ TestReporterParent, PortEvaluateParent, PortHandshakeParent, Base ],
     (base : ClassUnion<typeof TestReporterParent, typeof PortEvaluateParent, typeof PortHandshakeParent, typeof Base>) =>
 
-        class TestLauncherParent extends base implements TestLauncher {
-            @remote()
-            launchTest : (testDescriptorStr : string, checkInfo : SubTestCheckInfo) => Promise<any>
-
-            // @remote()
-            // getSameContextChildLauncher : () => Promise<TestLauncherChild>
-        }
-
+    class TestLauncherParent extends base implements TestLauncher {
+        @remote()
+        launchTest : (testDescriptorStr : string, checkInfo : SubTestCheckInfo) => Promise<any>
+    }
 ) {}
 
 
@@ -48,28 +32,21 @@ export class TestLauncherChild extends Mixin(
     [ TestReporterChild, PortEvaluateChild, PortHandshakeChild, Base ],
     (base : ClassUnion<typeof TestReporterChild, typeof PortEvaluateChild, typeof PortHandshakeChild, typeof Base>) =>
 
-        class TestLauncherChild extends base implements TestLauncher {
+    class TestLauncherChild extends base implements TestLauncher {
 
-            // @local()
-            // async getSameContextChildLauncher () : Promise<TestLauncherChild> {
-            //     // return this
-            // }
+        @local()
+        async launchTest (testDescriptorStr : string, checkInfo : SubTestCheckInfo) {
+            // there might be no `topTest` if test file does not contain any calls
+            // to static `it` method of any test class
+            const topTest = globalTestEnv.topTest || Test.new({
+                descriptor      : parse(testDescriptorStr)
+            })
 
+            topTest.reporter    = this
 
-            @local()
-            async launchTest (testDescriptorStr : string, checkInfo : SubTestCheckInfo) {
-                // there might be no `topTest` if test file does not contain any calls
-                // to static `it` method of any test class
-                const topTest = globalTestEnv.topTest || Test.new({
-                    descriptor      : parse(testDescriptorStr)
-                })
+            await topTest.start(checkInfo)
 
-                topTest.reporter    = this
-
-                await topTest.start(checkInfo)
-
-                globalTestEnv.clear()
-            }
+            globalTestEnv.clear()
         }
-
+    }
 ) {}
