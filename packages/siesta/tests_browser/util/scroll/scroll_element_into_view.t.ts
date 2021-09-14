30cf13a01cf54c8bb6fdd7db75052ca497b9e396
nickolay <nickolay8@gmail.com>
2021-09-14 17:39:18 +0300

    - extra test

--- packages/siesta/tests_browser/util/scroll/scroll_element_into_view.t.ts
+++ packages/siesta/tests_browser/util/scroll/scroll_element_into_view.t.ts
@@ -1,6 +1,53 @@
-import { describe } from "../../../browser.js"
+import { beforeEach, it, describe } from "../../../browser.js"
 import { isElementPointReachable, isElementPointVisible } from "../../../src/util_browser/Dom.js"
 import { scrollElementPointIntoView } from "../../../src/util_browser/Scroll.js"
+
+beforeEach(() => {
+    document.body.innerHTML = ''
+})
+
+
+it('Should be able to scroll element point into view, from right', async t => {
+    document.body.innerHTML =
+        '<div id="scrollable" style="position: absolute; left: 0; top: 0; background: blue; width: 200px; height: 200px; overflow: scroll">' +
+            '<div id="inner" style="position: absolute; left: 150px; top: 150px; width:100px; height: 100px; background: red"></div>' +
+        '</div>'
+
+    const scrollable    = t.$('#scrollable')
+    const inner         = t.$('#inner')
+
+    t.false(isElementPointVisible(inner, [ 50, 50 ]).visible, 'Point is initially scrolled out')
+
+    debugger
+
+    scrollElementPointIntoView(inner, [ 50, 50 ])
+
+    t.true(isElementPointVisible(inner, [ 50, 50 ]).visible, 'Point was scrolled in')
+})
+
+
+it('Should be able to scroll element point into view, from left', async t => {
+    document.body.innerHTML =
+        '<div id="scrollable" style="position: absolute; left: 0; top: 0; background: blue; width: 200px; height: 200px; overflow: scroll">' +
+            '<div id="inner" style="position: absolute; left: 150px; top: 150px; width:100px; height: 100px; background: red"></div>' +
+            '<div id="inner2" style="position: absolute; left: 550px; top: 550px; width:100px; height: 100px; background: red"></div>' +
+        '</div>'
+
+    const scrollable        = t.$('#scrollable')
+    const inner             = t.$('#inner')
+
+    scrollable.scrollLeft   = 250
+    scrollable.scrollTop    = 250
+
+    t.false(isElementPointVisible(inner, [ 50, 50 ]).visible, 'Point is initially scrolled out')
+
+    scrollElementPointIntoView(inner, [ 50, 50 ])
+
+    t.true(isElementPointVisible(inner, [ 50, 50 ]).visible, 'Point was scrolled in')
+})
+
+
+
 
 describe('Scroll target into view should work', async t => {
     let scrollable
