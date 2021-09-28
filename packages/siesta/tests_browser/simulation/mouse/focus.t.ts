c66d1fd43cca1179f8b1eb9e44d8cdbcc7bf9512
nickolay <nickolay8@gmail.com>
2021-09-28 15:49:14 +0300

    - progress on tests

--- packages/siesta/tests_browser/simulation/mouse/focus.t.ts
+++ packages/siesta/tests_browser/simulation/mouse/focus.t.ts
@@ -51,3 +51,29 @@ it('Should not trigger focus event on mousedown if el has disabled attribute', a
     await t.mouseDown('#div')
     await t.mouseUp()
 })
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+it('Should trigger focus/blur when clicking inside/outside of the text input', async t => {
+    // in FF default width of input is > 200px need provide explicit value
+    document.body.innerHTML = '<input style="width: 150px" id="inp" type="text" />'
+
+    t.willFireNTimes("#inp", 'focus', 1)
+    t.willFireNTimes("#inp", 'blur', 1)
+
+    await t.click('#inp')
+    await t.click('#inp', [ '100% + 10', '100% + 10' ])
+})
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+it('Should not trigger extra blur event when clicking on another text input', async t => {
+    document.body.innerHTML = '<input id="inp1" type="text" /><input id="inp2" type="text" />'
+
+    t.willFireNTimes("#inp1", 'focus', 1)
+    t.willFireNTimes("#inp1", 'blur', 1)
+    t.willFireNTimes("#inp2", 'focus', 1)
+
+    await t.click('#inp1')
+    await t.click('#inp2')
+})
