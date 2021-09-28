694b722582df25844f0a78405a09b6bba8a42762
nickolay <nickolay8@gmail.com>
2021-09-28 18:15:54 +0300

    - wait for target test

--- packages/siesta/tests_browser/simulation/mouse/wait_for_target.t.ts
+++ packages/siesta/tests_browser/simulation/mouse/wait_for_target.t.ts
@@ -1,4 +1,5 @@
 import { beforeEach, it, TestBrowser } from "../../../browser.js"
+import { delay } from "../../../src/util/TimeHelpers.js"
 import { createPositionedElement } from "../../@helpers.js"
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -9,7 +10,21 @@ beforeEach(async (t : TestBrowser) => {
 })
 
 
-const createDelayedDiv = (delay : number = 50) : HTMLElement => {
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// this measuring verifies that some waiting actually happened
+const measure = async <T>(promise : Promise<T>) : Promise<{ elapsed : number, resolved : T }> => {
+    const start     = Date.now()
+
+    const resolved  = await promise
+
+    return { elapsed : Date.now() - start, resolved }
+}
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+const defaultDelay  = 50
+
+const delayedDivCreation = (delay : number = defaultDelay) : HTMLElement => {
     const div       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
 
     div.style.backgroundColor = 'green'
@@ -20,336 +35,266 @@ const createDelayedDiv = (delay : number = 50) : HTMLElement => {
     return div
 }
 
+const delayedDisplayStyle = (delay : number = defaultDelay) : HTMLElement => {
+    const div       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
+
+    div.style.backgroundColor   = 'green'
+    div.style.display           = 'none'
+    div.id                      = 'delayed'
+
+    document.body.appendChild(div)
+
+    setTimeout(() => div.style.display = 'block', delay)
+
+    return div
+}
+
+const delayedVisibilityStyle = (delay : number = defaultDelay) : HTMLElement => {
+    const div       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
+
+    div.style.backgroundColor   = 'green'
+    div.style.visibility        = 'hidden'
+    div.id                      = 'delayed'
+
+    document.body.appendChild(div)
+
+    setTimeout(() => div.style.visibility = 'visible', delay)
+
+    return div
+}
+
+const delayedSize = (delay : number = defaultDelay) : HTMLElement => {
+    const div       = createPositionedElement('div', { left : 50, top : 50, width : 0, height : 0 })
+
+    div.style.backgroundColor   = 'green'
+    div.id                      = 'delayed'
+
+    document.body.appendChild(div)
+
+    setTimeout(() => {
+        div.style.width     = '100px'
+        div.style.height    = '100px'
+    }, delay)
+
+    return div
+}
+
+const delayedReachability = (delay : number = defaultDelay) : HTMLElement => {
+    const div       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
+
+    div.style.backgroundColor   = 'green'
+    div.id                      = 'delayed'
+    div.style.zIndex            = '100'
+
+    const mask       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
+    mask.style.backgroundColor  = 'blue'
+    mask.style.zIndex           = '1000'
+
+    document.body.appendChild(div)
+    document.body.appendChild(mask)
+
+    setTimeout(() => mask.remove(), delay)
+
+    return div
+}
+
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`mouseDown` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
+const basicTestingScenarios = (
+    name                : string,
+    createDelayedDiv    : (delay? : number) => HTMLElement,
+    skipMouseMove       : boolean = false
+) => {
 
-    t.firesOnce(div, 'mousedown')
+    it(name, async t => {
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`mouseDown` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
 
-    await t.mouseDown('#delayed')
-    await t.mouseUp()
-})
+            t.firesOnce(div, 'mousedown')
+
+            const { elapsed } = await measure(t.mouseDown('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+
+            await t.mouseUp()
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`mouseUp` should await for target to become actionable', async t => {
+            await t.mouseDown()
+
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'mouseup')
+
+            const { elapsed } = await measure(t.mouseUp('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        !skipMouseMove && t.it('`moveMouseTo` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.waitForEvent(div, 'mouseenter')
+
+            const { elapsed } = await measure(t.moveMouseTo('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`click` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'click')
+
+            const { elapsed } = await measure(t.click('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`rightClick` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'contextmenu')
+
+            div.addEventListener('contextmenu', e => e.preventDefault())
+
+            const { elapsed } = await measure(t.rightClick('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`doubleClick` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'dblclick')
+
+            const { elapsed } = await measure(t.doubleClick('#delayed'))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`dragTo` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'mousedown')
+
+            const { elapsed } = await measure(t.dragTo('#delayed', [ 300, 300 ]))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+
+
+        //⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼
+        t.it('`dragBy` should await for target to become actionable', async t => {
+            const div = createDelayedDiv()
+
+            t.firesOnce(div, 'mousedown')
+
+            const { elapsed } = await measure(t.dragBy('#delayed', [ 100, 100 ]))
+
+            t.isGreaterOrEqual(elapsed, defaultDelay)
+        })
+    })
+}
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`mouseUp` should await for target to become actionable', async t => {
-    await t.mouseDown()
-
-    const div = createDelayedDiv()
-
-    t.firesOnce(div, 'mouseup')
-
-    await t.mouseUp('#delayed')
-})
+basicTestingScenarios('Delayed creation', delayedDivCreation)
+basicTestingScenarios('Delayed display style', delayedDisplayStyle)
+basicTestingScenarios('Delayed visibility style', delayedVisibilityStyle)
+basicTestingScenarios('Delayed size', delayedSize)
+basicTestingScenarios('Delayed reachability', delayedReachability, true)
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`moveMouseTo` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
+it('`moveMouseTo` should NOT await for target to become reachable', async t => {
+    const div       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
 
-    t.waitForEvent(div, 'mousemove')
+    div.style.backgroundColor   = 'green'
+    div.id                      = 'delayed'
+    div.style.zIndex            = '100'
+
+    const mask       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
+    mask.style.backgroundColor  = 'blue'
+    mask.style.zIndex           = '1000'
+
+    document.body.appendChild(div)
+    document.body.appendChild(mask)
 
     await t.moveMouseTo('#delayed')
+
+    t.not.isElementPointReachable(div)
 })
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`click` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
+it('`dragTo` method  should wait for target', async t => {
+    const source       = createPositionedElement('div', { left : 50, top : 50, width : 100, height : 100 })
 
-    t.firesOnce(div, 'click')
+    source.style.backgroundColor    = 'green'
+    source.id                       = 'source'
 
-    await t.click('#delayed')
+    document.body.appendChild(source)
+
+    setTimeout(() => {
+        const target       = createPositionedElement('div', { left : 250, top : 50, width : 100, height : 100 })
+        target.id                       = 'target'
+        target.style.backgroundColor    = 'blue'
+
+        document.body.appendChild(target)
+
+        t.firesOnce('#target', 'mouseup')
+    }, defaultDelay)
+
+    t.firesOnce('#source', 'mousedown')
+
+    const { elapsed } = await measure(t.dragTo('#source', '#target'))
+
+    t.isGreaterOrEqual(elapsed, defaultDelay)
 })
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`rightClick` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
+it('Stress test: should handle moving targets', async t => {
+    const target      = createPositionedElement('div', { left : 0, top : 0, width : 50, height : 50 })
 
-    t.firesOnce(div, 'contextmenu')
+    target.style.backgroundColor    = 'green'
+    target.id                       = 'target'
 
-    div.addEventListener('contextmenu', e => e.preventDefault())
+    document.body.appendChild(target)
 
-    await t.rightClick('#delayed')
+    const positions = [
+        [ 0, 0 ],
+        [ 500, 0 ],
+        [ 500, 500 ],
+        [ 0, 500 ],
+        [ 0, 0 ]
+    ]
+
+    // Move the cursor away so it takes a bit of time to reach the button initially
+    await t.moveMouseTo(600, 600)
+
+    t.willFireNTimes(target, 'click', 1)
+
+    t.click('#target', { mouseMovePrecision : 1 })
+
+    for (const position of positions) {
+        target.style.left   = position[ 0 ] + 'px'
+        target.style.top    = position[ 1 ] + 'px'
+
+        await delay(300)
+    }
 })
-
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`rightClick` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
-
-    t.firesOnce(div, 'contextmenu')
-
-    div.addEventListener('contextmenu', e => e.preventDefault())
-
-    await t.rightClick('#delayed')
-})
-
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`doubleClick` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
-
-    t.firesOnce(div, 'dblclick')
-
-    await t.doubleClick('#delayed')
-})
-
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`dragTo` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
-
-    t.firesOnce(div, 'mousedown')
-
-    await t.dragTo('#delayed', [ 300, 300 ])
-})
-
-
-//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('`dragBy` should await for target to become actionable', async t => {
-    const div = createDelayedDiv()
-
-    t.firesOnce(div, 'mousedown')
-
-    await t.dragBy('#delayed', [ 100, 100 ])
-})
-
-
-// StartTest(async t => {
-//     t.describe('Basic tests', async t => {
-//
-//         t.it('dragBy', async t => {
-//
-//             let btn = new Ext.Button({ id : 'dragBy', text : 'dragBy' })
-//
-//             setTimeout(function () {
-//                 btn.render(Ext.getBody())
-//
-//                 t.firesOnce(btn.el, 'mousedown')
-//             }, 500)
-//
-//             t.chain(
-//                 { drag : '#dragBy', by : [2, 2] }
-//             )
-//         })
-//
-//
-//         t.it('dragTo - wait for source', async t => {
-//             let btn = new Ext.Button({ id : 'dragTo', text : 'dragTo' })
-//
-//             setTimeout(function () {
-//                 btn.render(Ext.getBody())
-//
-//                 t.firesOnce(btn.el, 'mousedown')
-//             }, 500)
-//
-//             t.chain(
-//                 { drag : '#dragTo', to : [2, 2] }
-//             )
-//         })
-//
-//
-//         t.it('dragTo - wait for source && target', async t => {
-//             let btnSource   = new Ext.Button({ id : 'dragSource', text : 'dragSource' })
-//             let btnTarget   = new Ext.Button({ id : 'dragTarget', text : 'dragTarget' })
-//
-//             setTimeout(function () {
-//                 btnSource.render(Ext.getBody())
-//
-//                 t.firesOnce(btnSource.el, 'mousedown')
-//
-//                 setTimeout(function () {
-//                     btnTarget.render(Ext.getBody())
-//
-//                     t.firesOnce(btnTarget.el, 'mouseup')
-//                 }, 250)
-//             }, 250)
-//
-//             t.chain(
-//                 { drag : '>>#dragSource', to : '>>#dragTarget' }
-//             )
-//         })
-//
-//
-//         t.it('Should wait until target el becomes top', async t => {
-//             let el1 = document.createElement('div')
-//
-//             el1.id             = 'one'
-//             el1.style.position = 'absolute'
-//             el1.style.left     = '0'
-//             el1.style.top      = '100px'
-//             el1.style.zIndex   = 5
-//             el1.innerHTML      = 'TEXT ONE'
-//
-//             document.body.appendChild(el1)
-//
-//             let el2 = document.createElement('div')
-//
-//             el2.id             = 'two'
-//             el2.style.position = 'absolute'
-//             el2.style.left     = '0'
-//             el2.style.top      = '100px'
-//             el2.style.zIndex   = 1
-//             el2.innerHTML      = 'TEXT TWO'
-//
-//             let el2Clicked = false
-//
-//             Ext.get(el2).on('click', function () {
-//                 el2Clicked = true
-//             })
-//
-//             let async = t.beginAsync()
-//
-//             setTimeout(function () {
-//                 document.body.appendChild(el2)
-//
-//                 setTimeout(function () {
-//                     t.notOk(el2Clicked, "Click not happened yet, even that `el2` is already in DOM")
-//
-//                     el2.style.zIndex = 10
-//
-//                     t.endAsync(async)
-//                 }, 500)
-//             }, 100)
-//
-//             t.chain(
-//                 { click : '#two' },
-//                 function () {
-//                     t.ok(el2Clicked, "Click registered")
-//                 }
-//             )
-//         })
-//
-//         t.it('Stress test: should handle moving targets', async t => {
-//             let button = new Ext.Button({
-//                 text     : "Click me",
-//                 id       : 'btn10',
-//                 floating : true,
-//                 renderTo : document.body
-//             })
-//
-//             let positions = [
-//                 [0, 0],
-//                 [500, 0],
-//                 [500, 500],
-//                 [0, 500],
-//                 [0, 0]
-//             ]
-//
-//             t.willFireNTimes(button, 'click', 1)
-//
-//             t.chain(
-//                 { moveCursorTo : [500, 500] },
-//
-//                 // Move the cursor away so it takes a bit of time to reach the button initially
-//                 function (next) {
-//                     button.setPosition(positions[0][0], positions[0][1])
-//                     t.click('#btn10', function () {})
-//
-//                     next()
-//                 },
-//
-//                 { waitFor : 150 },
-//
-//                 function (next) {
-//                     button.setPosition(positions[1][0], positions[1][1])
-//
-//                     next()
-//                 },
-//                 { waitFor : 150 },
-//
-//                 function (next) {
-//                     button.setPosition(positions[2][0], positions[2][1])
-//
-//                     next()
-//                 },
-//
-//                 { waitFor : 150 },
-//
-//                 function (next) {
-//                     button.setPosition(positions[3][0], positions[3][1])
-//
-//                     next()
-//                 },
-//                 { waitFor : 150 },
-//
-//                 function (next) {
-//                     button.setPosition(positions[4][0], positions[4][1])
-//
-//                     t.waitForEvent(button, 'click', next)
-//                 }
-//             )
-//         })
-//     })
-//
-//     t.describe('should handle temporarily unreachable targets', async t => {
-//         let panel, button
-//
-//         function scheduleMask (next) {
-//
-//             setTimeout(function () {
-//                 panel.mask('oops')
-//
-//                 setTimeout(function () {
-//                     panel.unmask()
-//                 }, 2000)
-//             }, 50)
-//
-//             next()
-//         }
-//
-//         t.beforeEach(function () {
-//             panel = panel && panel.destroy()
-//
-//             button = new Ext.Button({
-//                 text      : "Click me",
-//                 id        : 'btn',
-//                 draggable : true
-//             })
-//
-//             panel = new Ext.Panel({
-//                 style    : 'margin-top:100px',
-//                 buttons  : [button],
-//                 renderTo : document.body,
-//                 height   : 100,
-//                 width    : 100
-//             })
-//         })
-//
-//         t.it('should handle clicking temp unreachable targets', async t => {
-//             t.willFireNTimes(button, 'click', 1)
-//
-//             t.chain(
-//                 { moveCursorTo : [0, 0] },
-//                 scheduleMask,
-//                 { click : '#btn' }
-//             )
-//         })
-//
-//         t.it('should handle draggin temp unreachable targets', async t => {
-//
-//             t.firesAtLeastNTimes(button, 'move', 1)
-//
-//             t.chain(
-//                 { moveCursorTo : [0, 0] },
-//                 scheduleMask,
-//                 { drag : '#btn', to : [100, 40] }
-//             )
-//         })
-//
-//         t.it('should handle draggin temp unreachable targets', async t => {
-//
-//             t.firesAtLeastNTimes(button, 'move', 1)
-//
-//             t.chain(
-//                 { moveCursorTo : [0, 0] },
-//                 scheduleMask,
-//                 { drag : '#btn', by : [-10, 0] }
-//             )
-//         })
-//     })
-//
-// })
