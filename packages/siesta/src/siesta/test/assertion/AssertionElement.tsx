678b0715f671b75c883d04cca6af377732efb57b
nickolay <nickolay8@gmail.com>
2021-10-01 13:07:11 +0300

    - `elementValueIs` assertion (former `hasValue`)

--- packages/siesta/src/siesta/test/assertion/AssertionElement.tsx
+++ packages/siesta/src/siesta/test/assertion/AssertionElement.tsx
@@ -17,6 +17,19 @@ export class AssertionElement extends Mixin(
 
     class AssertionElement extends base {
 
+        addMissingTargetFailedAssertion (target : ActionTarget, name, description) {
+            this.addResult(Assertion.new({
+                name,
+                description,
+                passed      : false,
+
+                annotation  : <div>
+                    Action target <span>{ target }</span> did not resolve to DOM element
+                </div>
+            }))
+        }
+
+
         isElementPointReachable (
             target : ActionTarget, description? : string
         )
@@ -39,15 +52,8 @@ export class AssertionElement extends Mixin(
             const el            = this.resolveActionTarget(target)
 
             if (!el) {
-                this.addResult(Assertion.new({
-                    name        : 'isElementPointReachable',
-                    passed      : false,
-                    description,
-
-                    annotation  : <div>
-                        Action target <span>{ target }</span> did not resolve to DOM element
-                    </div>
-                }))
+                this.addMissingTargetFailedAssertion('isElementPointReachable', target, description)
+                return
             }
 
             const res           = isElementPointReachable(el, offset, allowChildren)
@@ -79,18 +85,24 @@ export class AssertionElement extends Mixin(
         }
 
 
-        // deprecated, backward compat only
-        elementIsTopElement (target : ActionTarget, allowChildren : boolean = false, description? : string, strict : boolean = false) {
-            const el    = this.resolveActionTarget(target)
+        elementValueIs (target : ActionTarget, value : string, description? : string) {
+            const el            = this.resolveActionTarget(target) as HTMLInputElement
 
-            if (strict) {
-                this.isElementPointReachable(el, { offset : [ 1, 1 ], allowChildren }, `Top/left corner: ${ description }`)
-                this.isElementPointReachable(el, { offset : [ 1, '100% - 1' ], allowChildren }, `Bottom/left corner: ${ description }`)
-                this.isElementPointReachable(el, { offset : [ '100% - 1', '100% - 1' ], allowChildren }, `Bottom/right corner: ${ description }`)
-                this.isElementPointReachable(el, { offset : [ '100% - 1', 1 ], allowChildren }, `top/right corner: ${ description }`)
-            } else {
-                this.isElementPointReachable(el, { offset : [ '50%', '50%' ], allowChildren }, `${ description }`)
+            if (!el) {
+                this.addMissingTargetFailedAssertion('elementValueIs', target, description)
+                return
             }
+
+            const foundValue    = el.value
+
+            this.assertEqualityInternal(
+                'elementValueIs',
+                this.comparePrimitivesIs(foundValue, value),
+                this.isAssertionNegated,
+                foundValue,
+                value,
+                description
+            )
         }
 
 
@@ -129,6 +141,27 @@ export class AssertionElement extends Mixin(
                         <div>Waiting for selector aborted by timeout ({ waitOptions.timeout }ms)</div>
                 }
             })
+        }
+
+
+        //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        // deprecated, backward compat only
+        elementIsTopElement (target : ActionTarget, allowChildren : boolean = false, description? : string, strict : boolean = false) {
+            const el    = this.resolveActionTarget(target)
+
+            if (strict) {
+                this.isElementPointReachable(el, { offset : [ 1, 1 ], allowChildren }, `Top/left corner: ${ description }`)
+                this.isElementPointReachable(el, { offset : [ 1, '100% - 1' ], allowChildren }, `Bottom/left corner: ${ description }`)
+                this.isElementPointReachable(el, { offset : [ '100% - 1', '100% - 1' ], allowChildren }, `Bottom/right corner: ${ description }`)
+                this.isElementPointReachable(el, { offset : [ '100% - 1', 1 ], allowChildren }, `top/right corner: ${ description }`)
+            } else {
+                this.isElementPointReachable(el, { offset : [ '50%', '50%' ], allowChildren }, `${ description }`)
+            }
+        }
+
+
+        hasValue (target : ActionTarget, value : string, description? : string) {
+            return this.elementValueIs(target, value, description)
         }
     }
 ) {}
