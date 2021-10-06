8bcc4b5ad8a16ee8f6374c708f6fe32d21306268
nickolay <nickolay8@gmail.com>
2021-10-06 17:57:02 +0300

    - a bit faster test

--- packages/siesta/tests_browser/simulation/mouse/wait_for_target.t.ts
+++ packages/siesta/tests_browser/simulation/mouse/wait_for_target.t.ts
@@ -278,7 +278,7 @@ it('Stress test: should handle moving targets', async t => {
 
     t.willFireNTimes(target, 'click', 1)
 
-    t.click('#target', { mouseMovePrecision : 1 })
+    t.click('#target', { mouseMovePrecision : 5 })
 
     for (const position of positions) {
         target.style.left   = position[ 0 ] + 'px'
