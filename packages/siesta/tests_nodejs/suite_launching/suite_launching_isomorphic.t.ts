504a66a74315e80fa091e6c0f185ebbe42342039
nickolay <nickolay8@gmail.com>
2021-10-15 15:06:34 +0300

    - progress on suite launching tests

--- packages/siesta/tests_nodejs/suite_launching/suite_launching_isomorphic.t.ts
+++ packages/siesta/tests_nodejs/suite_launching/suite_launching_isomorphic.t.ts
@@ -1,11 +1,12 @@
 import path from "path"
 import { fileURLToPath } from "url"
-import { it } from "../../nodejs.js"
+import { it, TestNodejs } from "../../nodejs.js"
 import {
+    LaunchResult,
     launchWebServer,
     runProjectDirectly,
     runProjectViaLauncher,
-    runTestDirectly,
+    runTestDirectly, runTestsQueued,
     verifySampleProjectLaunch,
     verifySampleTestLaunch
 } from "../@src/suite_launch_helpers.js"
@@ -16,38 +17,44 @@ const __dirname     = path.dirname(__filename)
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-it('Should be able to launch the isomorphic project in Node.js directly', async t => {
-    const launchRes     = await runProjectDirectly(path.resolve(__dirname, '../@sample_test_suites/isomorphic/index.js'))
-
-    await verifySampleProjectLaunch(t, launchRes)
-})
-
-
-it('Should be able to launch the isomorphic project in Node.js via launcher', async t => {
-    const launchRes     = await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomorphic/index.js'))
-
-    await verifySampleProjectLaunch(t, launchRes)
-})
-
-
-it('Should be able to launch the isomorphic test file in Node.js directly', async t => {
-    const launchRes     = await runTestDirectly(path.resolve(__dirname, '../@sample_test_suites/isomorphic/test_1.t.js'))
-
-    await verifySampleTestLaunch(t, launchRes)
-})
-
-
-it('Should be able to launch the isomorphic test file in Node.js via launcher', async t => {
-    const launchRes     = await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomorphic/test_1.t.js'))
-
-    await verifySampleTestLaunch(t, launchRes)
-})
-
-
-it('Should be able to launch the isomorphic test file in Node.js via launcher with glob', async t => {
-    const launchRes     = await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomo*/t*t_1.t.js'))
-
-    await verifySampleTestLaunch(t, launchRes)
+it('Launching isomorphic tests in Node.js should work', async t => {
+    await runTestsQueued(t, [
+        {
+            title       : 'Should be able to launch the isomorphic project in Node.js directly',
+            launch      : async () => await runProjectDirectly(path.resolve(__dirname, '../@sample_test_suites/isomorphic/index.js')),
+            func        : async (t : TestNodejs, launchRes : LaunchResult) => {
+                await verifySampleProjectLaunch(t, launchRes)
+            }
+        },
+        {
+            title       : 'Should be able to launch the isomorphic project in Node.js via launcher',
+            launch      : async () => await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomorphic/index.js')),
+            func        : async (t : TestNodejs, launchRes : LaunchResult) => {
+                await verifySampleProjectLaunch(t, launchRes)
+            }
+        },
+        {
+            title       : 'Should be able to launch the isomorphic test file in Node.js directly',
+            launch      : async () => await runTestDirectly(path.resolve(__dirname, '../@sample_test_suites/isomorphic/test_1.t.js')),
+            func        : async (t : TestNodejs, launchRes : LaunchResult) => {
+                await verifySampleTestLaunch(t, launchRes)
+            }
+        },
+        {
+            title       : 'Should be able to launch the isomorphic test file in Node.js via launcher',
+            launch      : async () => await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomorphic/test_1.t.js')),
+            func        : async (t : TestNodejs, launchRes : LaunchResult) => {
+                await verifySampleTestLaunch(t, launchRes)
+            }
+        },
+        {
+            title       : 'Should be able to launch the isomorphic test file in Node.js via launcher with glob',
+            launch      : async () => await runProjectViaLauncher(path.resolve(__dirname, '../@sample_test_suites/isomo*/t*t_1.t.js')),
+            func        : async (t : TestNodejs, launchRes : LaunchResult) => {
+                await verifySampleTestLaunch(t, launchRes)
+            }
+        },
+    ])
 })
 
 
