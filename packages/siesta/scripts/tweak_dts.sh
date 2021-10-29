e43489c04fcbc4667075ddaceb7e92a111c3d804
nickolay <nickolay8@gmail.com>
2021-10-29 12:08:38 +0300

    - dts mess

--- packages/siesta/scripts/tweak_dts.sh
+++ packages/siesta/scripts/tweak_dts.sh
@@ -10,6 +10,12 @@ cd "$DIR/.."
 
 echo ">> Tweaking *.d.ts files"
 
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Very very horrible mess caused by: https://github.com/microsoft/TypeScript/issues/35822
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Isomorphic
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 sed -i -e 's!import { DashboardLaunchInfo }.*!type DashboardLaunchInfo = any!' "src/siesta/test/Test.d.ts"
 sed -i -e 's!import { Launcher }.*!declare const Launcher : any!' "src/siesta/test/Test.d.ts"
 sed -i -e 's!import { TestLauncherChild }.*!type TestLauncherChild = any!' "src/siesta/test/Test.d.ts"
@@ -17,3 +23,42 @@ sed -i -e 's!import { TestLauncherChild }.*!type TestLauncherChild = any!' "src/
 sed -i -e 's!import { Launcher }.*!declare const Launcher : any!' "src/siesta/project/Project.d.ts"
 
 sed -i -e 's!fromTestResult.*!!' "src/siesta/test/TestResult.d.ts"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Browser
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+sed -i -e 's!import { Launcher }.*!declare const Launcher : any!' "src/siesta/test/TestBrowser.d.ts"
+sed -i -e 's!import { TestLauncherChild }.*!type TestLauncherChild = any!' "src/siesta/test/TestBrowser.d.ts"
+sed -i -e 's!import("\.\./launcher/DashboardConnector\.js")\.DashboardLaunchInfo!any!' "src/siesta/test/TestBrowser.d.ts"
+
+sed -i -e 's!typeof import("\.\./launcher/Launcher\.js").Launcher!any!' "src/siesta/project/ProjectBrowser.d.ts"
+sed -i -e 's!import("\.\./launcher/Launcher\.js").Launcher!any!' "src/siesta/project/ProjectBrowser.d.ts"
+
+sed -i -e 's!connector:.*!connector:any!' "src/siesta/simulate/UserAgent.d.ts"
+sed -i -e 's!import("\.\./launcher/DashboardConnector\.js")\.DashboardLaunchInfo!any!' "src/siesta/simulate/UserAgent.d.ts"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Node.js
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+sed -i -e 's!import { Launcher }.*!declare const Launcher : any!' "src/siesta/test/TestNodejs.d.ts"
+sed -i -e 's!import("\.\./launcher/DashboardConnector\.js")\.DashboardLaunchInfo!any!' "src/siesta/test/TestNodejs.d.ts"
+sed -i -e 's!connector:.*!connector:any!' "src/siesta/test/TestNodejs.d.ts"
+
+sed -i -e 's!typeof import("\.\./launcher/Launcher\.js").Launcher!any!' "src/siesta/project/ProjectNodejs.d.ts"
+sed -i -e 's!import("\.\./launcher/Launcher\.js").Launcher!any!' "src/siesta/project/ProjectNodejs.d.ts"
+
+sed -i -e 's!launcherClass:.*!launcherClass:any;!' "src/siesta/project/ProjectNodejs.d.ts"
+
+sed -i -e 's!import { LauncherTerminal }.*!declare const LauncherTerminal : any!' "src/siesta/project/ProjectTerminal.d.ts"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Deno.js
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+sed -i -e 's!import { Launcher }.*!declare const Launcher : any!' "src/siesta/test/TestDeno.d.ts"
+sed -i -e 's!import("\.\./launcher/DashboardConnector\.js")\.DashboardLaunchInfo!any!' "src/siesta/test/TestDeno.d.ts"
+sed -i -e 's!connector:.*!connector:any!' "src/siesta/test/TestDeno.d.ts"
+sed -i -e 's!dashboardLaunchInfo:.*!dashboardLaunchInfo:any!' "src/siesta/test/TestDeno.d.ts"
+
+sed -i -e 's!getLauncherClass():.*!getLauncherClass():any!' "src/siesta/project/ProjectDeno.d.ts"
+sed -i -e 's!getStandaloneLauncher():.*!getStandaloneLauncher():any!' "src/siesta/project/ProjectDeno.d.ts"
+sed -i -e 's!launcherClass:.*!launcherClass:any!' "src/siesta/project/ProjectDeno.d.ts"
