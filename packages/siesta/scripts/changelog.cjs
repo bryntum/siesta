de2a47c20d6bc8f4a3ad2c253c92e62316881e1c
nickolay <nickolay8@gmail.com>
2021-10-29 13:54:14 +0300

    - release process

--- packages/siesta/scripts/changelog.cjs
+++ packages/siesta/scripts/changelog.cjs
@@ -7,30 +7,20 @@ const prependZero = (int, minLength) => {
     return '0000000'.slice(0, minLength - str.length) + str
 }
 
-const version       = require('../package.json').version
+const defaultVersion        = require('../package.json').version
 
-const updateVersion = () => {
+const updateVersion = (newVersion) => {
     const versionTime       = new Date()
     const changelog         = fs.readFileSync('CHANGELOG.md', 'utf8')
 
     const lines             = changelog.split('\n')
 
-    lines.splice(2, 0, getVersionStr(version, versionTime))
+    const version           = newVersion || defaultVersion
+
+    lines.splice(2, 0, getVersionStr(version, versionTime), '')
 
     fs.writeFileSync('CHANGELOG.md', lines.join('\n'), 'utf8')
 }
-
-
-// const updateVersionAndStartNew = (newVersion, newVersionTime) => {
-//     let changelog = fs.readFileSync('CHANGELOG.md', 'utf8')
-//
-//     const version   = (newVersion || version).replace(/^v/, '')
-//     const time      = new Date(newVersionTime) || new Date()
-//
-//     changelog = changelog.replace(/^(.*?)\{\{ \$NEXT \}\}/m, `$1{{ $NEXT }}\n\n$1${ getVersionStr(version, time) }`)
-//
-//     fs.writeFileSync('CHANGELOG.md', changelog, 'utf8')
-// }
 
 const getVersionStr = (version, now) => {
     return `## ${version}        ${ now.getFullYear() }-${ prependZero(now.getMonth() + 1, 2) }-${ prependZero(now.getDate(), 2) } ${ prependZero(now.getHours(), 2) }:${ prependZero(now.getMinutes(), 2) }`
@@ -38,6 +28,4 @@ const getVersionStr = (version, now) => {
 
 module.exports = {
     updateVersion
-    // ,
-    // updateVersionAndStartNew
 }
--- packages/siesta/scripts/publish.sh
+++ packages/siesta/scripts/publish.sh
@@ -12,28 +12,21 @@ if [[ -z "$V" ]]; then
 fi
 
 # bump version in distribution - won't be reflected in main repo, since "make_dist" removes the ".git"
-NEW_VERSION=$(npm version $V)
+NEW_VERSION=$(npm --no-git-tag-version version $V)
 
-NEW_VERSION_TIME=$(node -e "require('./scripts/changelog.cjs').updateVersion()")
-
-echo "/scripts" > .npmignore
-
-npm publish --access public
-
-#---------------------------------------------------------------------------------
-# post-publish steps, the following code is executed on the main repo
-cd "$DIR/../.."
-
-# update the changelog first, so that the tag, created with `npm version` below
-# will point to correct revision
-node -e "require('./scripts/changelog.cjs').updateVersionAndStartNew('$NEW_VERSION', $NEW_VERSION_TIME)"
+node -e "require('./scripts/changelog.cjs').updateVersion('$NEW_VERSION')"
 
 git add CHANGELOG.md
 git commit -m "Updated changelog"
 
-# bump version in main repo
-npm version $V
+git tag "v$NEW_VERSION"
 
+echo "/scripts" > .npmignore
+
+npm publish --access public --dry-run
+
+#---------------------------------------------------------------------------------
+# post-publish steps
 git push origin HEAD --tags
 
-scripts/publish_docs.sh "$DIR/../docs"
+scripts/publish_docs.sh "./docs"
