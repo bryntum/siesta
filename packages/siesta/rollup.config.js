1b71c0388e2d2dd66bcc2123d54cb20024037f84
nickolay <nickolay8@gmail.com>
2021-12-07 12:00:32 +0300

    - entry points and gitignore for new entries

--- packages/siesta/rollup.config.js
+++ packages/siesta/rollup.config.js
@@ -22,6 +22,8 @@ export default defineConfig({
         'browser.js',
         'nodejs.js',
         'deno.js',
+        'react.js',
+        'sencha.js',
         'resources/dashboard/index.js',
         // TODO ideally, for publishing, we should create a bundle w/o tests as entry points
         // however, how to test the package then? (we would like to run all the tests on the "bundled"
