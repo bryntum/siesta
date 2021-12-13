18802167f812f9bb8dd31fa0afb854a251c820e2
nickolay <nickolay8@gmail.com>
2021-12-13 17:20:45 +0300

    - extra coverage for type

--- packages/siesta/tests_browser/sencha/assertion/assertion_form_field.t.ts
+++ packages/siesta/tests_browser/sencha/assertion/assertion_form_field.t.ts
@@ -28,6 +28,26 @@ it('`fieldHasValue` assertion should work', async t => {
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+it('Should be able to type in the field with regexp validation', async t => {
+    const tf = new Ext.form.field.Text({
+        renderTo   : Ext.getBody(),
+        name       : 'name',
+        fieldLabel : 'Name',
+
+        maskRe     : /[\d\-]/,
+        regex      : /^\d{3}-\d{3}-\d{4}$/,
+
+        regexText  : 'Must be in the format xxx-xxx-xxxx'
+    })
+
+    await t.type(tf, 'Abracadabra123-456-7890')
+
+    t.fieldHasValue('[name=name]', "123-456-7890")
+})
+
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 it('`setValue` method should work for text field', async t => {
     const textField = new Ext.form.field.Text({
         renderTo    : document.body,
