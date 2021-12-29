056f781723596b1edb5efd6a4dd829417553fe4b
nickolay <nickolay8@gmail.com>
2021-12-29 12:14:46 +0300

    - backward compat

--- packages/siesta/index.ts
+++ packages/siesta/index.ts
@@ -1,5 +1,11 @@
 export { Test, it, iit, xit, describe, ddescribe, xdescribe, beforeEach, afterEach, expect } from "./src/siesta/test/Test.js"
+import { Test, it, iit, xit, describe, ddescribe, xdescribe, beforeEach, afterEach, expect } from "./src/siesta/test/Test.js"
 
 export { Project } from "./src/siesta/project/Project.js"
 
 export const siestaPackageRootUrl : string = import.meta.url.replace(/[^/]*$/, '')
+
+// backward compat
+export const StartTest = (func : (t : Test) => any) => {
+    return it('Root', func)
+}
