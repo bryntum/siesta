f1a04ec884654e197ed18f88c4426248785723f2
nickolay <nickolay8@gmail.com>
2022-01-25 12:13:03 +0300

    - style inheritance for streamed rendering

--- packages/siesta/tests_nodejs/jsx/streaming_rendering.t.tsx
+++ packages/siesta/tests_nodejs/jsx/streaming_rendering.t.tsx
@@ -5,19 +5,27 @@ import { TextJSX } from "../../src/jsx/TextJSX.js"
 import { XmlRendererStreaming } from "../../src/jsx/XmlRenderer.js"
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-const renderer      = XmlRendererStreaming.new({ colorerClass : ColorerNodejs })
+const renderer      = XmlRendererStreaming.new({
+    colorerClass    : ColorerNodejs,
+    styles          : new Map([
+        [ 'red', style => style.color = [ 255, 0, 0 ] ]
+    ])
+})
 
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 it('Should correctly render styled elements', async t => {
-
     t.is(
         renderer.render(<div>
             Some text <span class="underlined">underlined</span>
         </div>),
         `Some text ${ chalk.underline('underlined') }`
     )
+})
 
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+it('Should correctly combine styling and indentation', async t => {
     t.is(
         renderer.render(<div class="indented">
             Some text
@@ -26,18 +34,23 @@ it('Should correctly render styled elements', async t => {
 `  Some text
   ${ chalk.underline('underlined') }`
     )
+})
+
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+it('Should inherit styling from parent element', async t => {
 
     t.is(
-        renderer.render(<div class="indented">
+        renderer.render(<div class="indented red">
             Some text
             <div class="underlined">
                 <p>underlined1</p>
                 <p>underlined2</p>
             </div>
         </div>),
-`  Some text
-  ${ chalk.underline('underlined1') }
-  ${ chalk.underline('underlined2') }`
+`  ${ chalk.rgb(255, 0, 0)('Some text') }
+  ${ chalk.underline.rgb(255, 0, 0)('underlined1') }
+  ${ chalk.underline.rgb(255, 0, 0)('underlined2') }`
     )
 })
 
